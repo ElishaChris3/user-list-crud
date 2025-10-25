355c64dc5dd4dbf1e9e7cce71d07abaf52fb8e4f
ElishaChris3 <elishachris4@gmail.com>
2025-10-25 20:34:25 +0500

    package.jason i updated

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "prisma/config";
-import * as dotenv from 'dotenv';
-import * as path from 'path';
+// import { defineConfig } from "prisma/config";
+// import * as dotenv from 'dotenv';
+// import * as path from 'path';
 
-// Load .env file before accessing environment variables
-dotenv.config({ path: path.resolve(process.cwd(), '.env') });
+// // Load .env file before accessing environment variables
+// dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  engine: "classic",
-  datasource: {
-    url: process.env.DATABASE_URL || "",
-  },
-});
+// export default defineConfig({
+//   schema: "prisma/schema.prisma",
+//   engine: "classic",
+//   datasource: {
+//     url: process.env.DATABASE_URL || "",
+//   },
+// });
